6ab7c1834ca8151e145bd806963d6b1879516a13
Austin Kammerer <austin4191@gmail.com>
2021-10-16 11:09:57 -0500

    fixed sql syntax error

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "tasklist"
-    ("id" SERIAL PRIMARY KEY
+    ("id" SERIAL PRIMARY KEY,
     "task" VARCHAR(255) NOT NULL,
     "is_complete" BOOLEAN DEFAULT false,
     "time_comleted" TIMESTAMP);
