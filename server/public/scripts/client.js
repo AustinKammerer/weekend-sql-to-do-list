d743b77fa339912b7df746be4d7c51ee8d6cf2a3
Austin Kammerer <austin4191@gmail.com>
2021-10-16 13:45:06 -0500

    render function for tasks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -25,10 +25,38 @@ function refreshTasks() {
   })
     .then((res) => {
       console.log(res);
-      //   renderTasks(res);
-      $("#taskIn").val("");
+      renderTasks(res);
     })
     .catch((err) => {
       console.log("Error connecting to server:", err);
     });
+}
+
+function renderTasks(taskList) {
+  $("#taskIn").val("");
+  $("#taskListContainer").empty();
+  let timeCompleted = ``;
+  for (let task of taskList) {
+    if (task.is_complete) {
+      timeCompleted = task.task_completed;
+    }
+    let taskEntry = $(`
+        <div class="row">
+            <div class="col">
+                <p class="taskOut">${task.task}</p>
+            </div>
+            <div class="col">
+                <p class="timeCompletedOut">${timeCompleted}</p>
+            </div>
+            <div class="col">
+                <button class="markCompleteBtn btn btn-outline-success">Complete</button>
+            </div>
+            <div class="col">
+                <button class="deleteBtn btn btn-outline-danger">Delete</button>
+            </div>
+        </div>`);
+    taskEntry.data("id", task.id);
+    console.log(taskEntry.data("id"));
+    $("#taskListContainer").append(taskEntry);
+  }
 }
