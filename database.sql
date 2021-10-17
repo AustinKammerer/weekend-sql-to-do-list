426ada2b25786b4fadff81052fb48cb6218cbe5e
Austin Kammerer <austin4191@gmail.com>
2021-10-17 00:24:29 -0500

    added time_completed to table, timestamp function, and trigger on row update

--- database.sql
+++ database.sql
@@ -1,7 +1,24 @@
 CREATE TABLE "tasklist"
     ("id" SERIAL PRIMARY KEY,
     "task" VARCHAR(255) NOT NULL,
-    "is_complete" BOOLEAN DEFAULT false;
+    "is_complete" BOOLEAN DEFAULT false,
+    "time_completed" TIMESTAMP WITH TIME ZONE);
+
+
+-- trigger for time_completed found on https://stackoverflow.com/questions/1035980/update-timestamp-when-row-is-updated-in-postgresql
+-- creates a function that will set the time_completed column in a given row to the current timestamp
+CREATE OR REPLACE FUNCTION update_time_completed_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.time_completed = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- creates a trigger that will trigger the function when a column in the given row is updated
+CREATE TRIGGER update_tasklist_time_completed BEFORE UPDATE
+    ON "tasklist" FOR EACH ROW EXECUTE PROCEDURE 
+    update_time_completed_column();
 
 -- REFERENCE:
 
