15ee5103c185defddec4b5c34392c6db993ac713
Austin Kammerer <austin4191@gmail.com>
2021-10-17 00:19:41 -0500

    added TZ to sql timestamp

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE "tasklist"
     ("id" SERIAL PRIMARY KEY,
     "task" VARCHAR(255) NOT NULL,
     "is_complete" BOOLEAN DEFAULT false,
-    "time_completed" TIMESTAMP);
+    "time_completed" TIMESTAMP WITH TIME ZONE);
 
 
 -- trigger for time_completed found on https://stackoverflow.com/questions/1035980/update-timestamp-when-row-is-updated-in-postgresql
