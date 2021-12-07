2ed1a368a02814cea2b007e7a8b1f6164f3f929b
Austin Kammerer <austin4191@gmail.com>
2021-12-06 19:21:16 -0600

    config pg for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,15 +1,35 @@
-// import pg from node_modules
 const pg = require("pg");
+const url = require("url");
+let config = {};
 
-// set up pg to connect to the database
-const config = {
-  database: "weekend-to-do-app",
-  host: "localhost",
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000,
-};
-const pool = new pg.Pool(config);
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  let params = url.parse(process.env.DATABASE_URL);
+  let auth = params.auth.split(":");
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split("/")[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
+  // only change the things on the right side of the ||
+  config = {
+    user: process.env.PG_USER || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER || "localhost", // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || "weekend-to-do-app", //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
 
 // server terminal logs on connection success/failure
 pool.on("connect", () => {
@@ -19,5 +39,4 @@ pool.on("error", (err) => {
   console.log("error connecting to database", err);
 });
 
-// export the module for router
-module.exports = pool;
+module.exports = new pg.Pool(config);
